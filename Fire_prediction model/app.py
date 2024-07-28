9d640233a2ecd54b4673c71a6e31f777123f28d0
Atul Ranjan jha <99534159+AtulRanjanJha@users.noreply.github.com>
2024-07-28 10:37:13 +0530

    Add files via upload
    This model uses the Logistic Regression model to Predict wildfire occurrence based on the historical data the model is trained on.

---
+++ Fire_prediction model/app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output>str(0.5):
+        return render_template('index.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
+    else:
+        return render_template('index.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
